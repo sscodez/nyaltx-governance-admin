a53ec0a752f7cfcfea647e352d84561dd3baea39
ledgerswapio-ship-it <ssameershah1200@gmail.com>
2026-01-08 21:52:30 +0500

    first commit

--- README.md
+++ README.md
@@ -37,3 +37,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 # nyaltx-governance-admin
 # nyaltx-governance-admin
 # nyaltx-governance-admin
+# nyaltx-governance-admin
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,36 @@
+import type { StaticImageData } from 'next/image'
+
 declare module 'google-maps-react'
 declare module 'react-easy-edit'
+
+declare module '*.jpg' {
+  const content: StaticImageData
+  export default content
+}
+
+declare module '*.png' {
+  const content: StaticImageData
+  export default content
+}
+
+declare module '*.jpeg' {
+  const content: StaticImageData
+  export default content
+}
+
+declare module '*.gif' {
+  const content: StaticImageData
+  export default content
+}
+
+declare module '*.svg' {
+  const content: StaticImageData
+  export default content
+}
+
+declare module '*.webp' {
+  const content: StaticImageData
+  export default content
+}
 
 // declare module "react-table";
